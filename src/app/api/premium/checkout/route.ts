9f7bfa255d16a65c22076797fbf1b2a06c8d8859
Abdallh Elzorkany <abdoh5479@gmail.com>
2026-02-06 14:50:13 +0200

    ..

--- src/app/api/premium/checkout/route.ts
+++ src/app/api/premium/checkout/route.ts
@@ -1,3 +1,6 @@
 export async function POST(request: Request) {
   return Response.json({ message: "Hello World" });
 }
+export async function GET() {
+  return Response.json({ message: "Hello World" });
+}
